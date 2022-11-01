1493976592987c4a377c81cfaf4b27c1d636ac8b
Alexandra Chernova <chernova.aleksandra28@gmail.com>
2022-11-01 14:34:10 +0300

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 1_000;
+        int oneMile = 20;
+        int bonus = cost / oneMile;
+        System.out.println("Вам начислено " + bonus + " бонусных миль!");
+        System.out.println("Спасибо, что выбрали нашу авиакомпанию!");
+    }
+}
